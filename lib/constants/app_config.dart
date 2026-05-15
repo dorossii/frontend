9957b7a49a6d586ef87de3fb7ca6d3d522979a88
haruna1256 <haru_kawagishi@icloud.com>
2026-05-15 15:24:02 +0900

    [add] mock link

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -7,3 +7,17 @@ class AppConfig {
   static const String tokenEndpoint = '/auth/token';
   static const String appEndpoint = '/app/authed';
 }
+
+// モックのAPIレスポンスを定義
+class MockApiResponse {
+    static const String mockDomain = 'mock-dorossii.mattuu.com';
+    static String get baseUrl => 'https://$mockDomain';
+
+    // User
+    // トップ画面ユーザーステータスの取得
+    static const String topStatusEndpoint = '/app/user/status';
+    // Friend
+    // フレンドリストの取得
+    static const String friendListEndpoint = '/app/friend';
+
+}
