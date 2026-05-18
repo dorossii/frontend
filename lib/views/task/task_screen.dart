53385b45a3a15cdf7d8a39f111ba146389fa6f79
saya5140424 <tokunosima8008@gmail.com>
2026-05-18 16:26:57 +0900

    変更

--- lib/views/task/task_screen.dart
+++ lib/views/task/task_screen.dart
@@ -2,20 +2,13 @@ import 'package:flutter/material.dart';
 import 'task_view.dart';
 import 'task_view_model.dart';
 
-class TaskScreen extends StatefulWidget {
+class TaskScreen extends StatelessWidget {
   const TaskScreen({super.key});
 
   @override
-  State<TaskScreen> createState() => _TaskScreen();
-}
+  Widget build(BuildContext context) {
+    final viewModel = TaskViewModel();
 
-class _TaskScreen extends State<TaskScreen> {
-  
-  Future<void> _handleSort() async {
-
+    return TaskView(viewModel: viewModel);
   }
-  
-
-  @override
-  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
