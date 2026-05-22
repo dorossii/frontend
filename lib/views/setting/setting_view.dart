84b1c2b5a20f16d1cc049d24adbb981cf6ef1956
haruna1256 <haru_kawagishi@icloud.com>
2026-05-22 13:50:22 +0900

    [add] タスクスプラッシュの確認のために、ナビゲーションボタンを設定に配置

--- lib/views/setting/setting_view.dart
+++ lib/views/setting/setting_view.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import '../task/splash/task_splash_1.dart';
 
 class SettingsView extends StatelessWidget {
-
   final VoidCallback onLogoutPressed;
 
-  const SettingsView({
-    super.key,
-    required this.onLogoutPressed,
-  });
+  const SettingsView({super.key, required this.onLogoutPressed});
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-
         ListTile(
           leading: const Icon(Icons.logout),
           title: const Text('ログアウト'),
 
           onTap: onLogoutPressed,
+        ),
+        ElevatedButton(
+          child: Text("タスクスプラッシュのテスト"),
+          onPressed: () {
+            // ここにボタンを押した時に呼ばれるコードを書く
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TaskSplash1()),
+            );
+          },
         ),
       ],
     );
