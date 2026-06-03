ab0ed6a4db50e2b5f1cff2e7a46e1778e121543f
haruna1256 <haru_kawagishi@icloud.com>
2026-06-03 09:59:43 +0900

    [add] タイトル部分でのデフォルトでのpaddingをゼロに

--- lib/views/component/rescue/rescue_view.dart
+++ lib/views/component/rescue/rescue_view.dart
@@ -16,26 +16,49 @@ class RescueView {
         return StatefulBuilder(
           builder: (context, setState) {
             return AlertDialog(
-              title: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "誰をレスキューしますか？",
-                      style: TextStyle(
-                        fontFamily: 'textFont',
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.text,
-                        fontSize: 14,
+              backgroundColor: AppColors.subWhiteBackground,
+              titlePadding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: const BorderSide(color: AppColors.edgew, width: 3),
+              ),
+              title: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 16,
+                ),
+                decoration: const BoxDecoration(
+                  color: AppColors.sub,
+                  border: Border(
+                    bottom: BorderSide(color: AppColors.edgew, width: 2),
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        "誰をレスキューしますか？",
+                        style: TextStyle(
+                          fontFamily: 'textFont',
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.text,
+                          fontSize: 14,
+                        ),
                       ),
                     ),
-                  ),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    constraints: const BoxConstraints(),
-                    icon: const Icon(Icons.close, color: AppColors.text),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                ],
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(),
+                      icon: const Icon(Icons.close, color: AppColors.text),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
               ),
               content: SizedBox(
                 width: double.maxFinite,
