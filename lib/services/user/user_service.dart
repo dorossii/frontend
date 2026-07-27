c8c41d54c186a8913701cd0c2d2a13e9ef443622
haruna1256 <haru_kawagishi@icloud.com>
2026-07-27 14:36:20 +0900

    [update] モックから本番環境に移行

--- lib/services/user/user_service.dart
+++ lib/services/user/user_service.dart
@@ -1,34 +1,20 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-
 import '../../../models/user_status.dart';
 import '../../constants/app_config.dart';
+import '../auth_manager.dart';
 
 // ユーザーのステータスを取得する
 class UserService {
-  /// API URL
-  static const String url =
-      MockApiResponse.baseUrl + MockApiResponse.topStatusEndpoint;
-
-  /// 認証トークン
-  static const String token = 'mock-token-super-secret';
-
-  /// ユーザー情報取得
   Future<UserStatus> fetchUserStatus() async {
-    /// GET通信
-    final response = await http.get(
-      Uri.parse(url),
-
-      headers: {'accept': 'application/json', 'Authorization': token},
+  // ユーザー情報取得
+  final response = await AuthManager.authenticatedRequest(
+      AppConfig.topStatusEndpoint,
+      method: 'GET',
     );
-
-    /// 通信成功
-    if (response.statusCode == 200) {
-      /// JSON変換
+  
+  if (response.statusCode == 200) {
       final Map<String, dynamic> jsonData = jsonDecode(response.body);
-
-      /// Modelへ変換
       return UserStatus.fromJson(jsonData);
     }
 
